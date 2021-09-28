e1be7304738fd0efc2b473b87f3f67ebb7552db8
ph47s74x <32150884+ph47s74x@users.noreply.github.com>
2021-09-27 20:17:45 -0400

    install zsh mount shared folders restart vm tools

---
+++ install-zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
